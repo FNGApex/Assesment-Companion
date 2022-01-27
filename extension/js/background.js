eee3a1495bbcbb5dc7750a7fd58534e9c7ab0f5e
Apex <dr.gorodnov@gmail.com>
2022-01-27 14:22:02 -0800

    Extension edits

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,32 +1,33 @@
 /*
- * Lockdown the chrome browser
- *
- * Handles the following cases:
- * Case 1: Changing Tab/Window Focus
- * Case 2: Attempting to open new tab or window
- * Case 3: Closing Existing Tab Before Finishing
- * Case 4: Resizing Window
- * Case 5: Updating existing url
- *
+ * Allow to take Contect Assesment in the Firefox browser
  */
-let targetTabId;
-//let targetWindowId;
-//let targetUrl;
-//let targetEnabled = false;
-//let intervalHandle;
-// handle case when windowId or tabId changes
-// browser.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
+let windowStatus = false
+browser.runtime.onMessageExternal.addListener(function (request, sender, sendResponse){
+  console.log("I heard Something");
+});
+// browser.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
 //   switch (request.method) {
-//     case "assessment_started":
+//     case "extension_started":
+//       alert("Extension Started");
+//       windowStatus = true;
+//       sendResponse({
+//         message:"success"
+//       })
 //       break;
-//     case "assessment_ended":
+//     case "extension_ended":
+//       alert("Extension Ended")
+//       windowStatus = false;
+//       sendResponse({
+//         message: "success"
+//       });
 //       break;
-//     case "assessment_status":
-//       sendResponse({message: true});
+//     case "extension_status":
+//       alert("Status"+windowStatus);
+//       sendResponse({
+//         message: true
+//       });
 //       return;
 //     default:
-//       // Don't respond to unknown messages
 //       return;
 //   }
-//   sendResponse({message: "success"});
 // });
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let buttons = document.getElementsByTagName('button');
 let textarea = document.getElementById("textarea");
-function browser (){}
+extensionId = "assesment-companion@summitlearning.com"
+let extensionResponse = 0
 buttons[0].addEventListener("click", function (e) {
-  textarea.innerText = "Send Message";
+  textarea.innerText = "Sent Start Message";
+  window.postMessage({direction: "from-page-script",message:"extension_started"},"*");
 });
 buttons[1].addEventListener("click", function (e) {
-  textarea.innerText = "Status";
+  textarea.innerText = "Status Message:";
 });
 buttons[2].addEventListener("click", function (e) {
-  textarea.innerText = "Ending";
+  textarea.innerText = "Sent End Message";
+});
+window.addEventListener("message", function(event){
+  console.log(event);
 });
