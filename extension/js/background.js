dd862fd71d469167f7d181deedb7bbdd3aee6b93
Apex <dr.gorodnov@gmail.com>
2022-02-17 14:40:47 -0800

    Updates + First Stable

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,33 +1,4 @@
-/*
- * Allow to take Contect Assesment in the Firefox browser
- */
-let windowStatus = false
-browser.runtime.onMessageExternal.addListener(function (request, sender, sendResponse){
-  console.log("I heard Something");
+browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  sendResponse("Locking");
+  
 });
-// browser.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
-//   switch (request.method) {
-//     case "extension_started":
-//       alert("Extension Started");
-//       windowStatus = true;
-//       sendResponse({
-//         message:"success"
-//       })
-//       break;
-//     case "extension_ended":
-//       alert("Extension Ended")
-//       windowStatus = false;
-//       sendResponse({
-//         message: "success"
-//       });
-//       break;
-//     case "extension_status":
-//       alert("Status"+windowStatus);
-//       sendResponse({
-//         message: true
-//       });
-//       return;
-//     default:
-//       return;
-//   }
-// });
--- extension/js/contentscript.js
+++ extension/js/contentscript.js
@@ -1,1 +1,25 @@
-document.body.innerHTML = ""
+let buttons = document.getElementsByTagName('button');
+let textarea = document.getElementById("textarea");
+extensionId = "assesment-companion@summitlearning.com"
+let extensionResponse = 0
+let sending;
+
+buttons[0].addEventListener("click", function (e) {
+  textarea.innerText = "Sent Start Message";
+  sending = browser.runtime.sendMessage({
+    request : "lockBrowser"
+  });
+  sending.then(function (response) {
+    textarea.innerText = response;
+  }, function (error) {
+    textarea.innerText = "Error" + error;
+  })
+});
+
+
+buttons[1].addEventListener("click", function (e) {
+  textarea.innerText = "Status Message:";
+});
+buttons[2].addEventListener("click", function (e) {
+  textarea.innerText = "Sent End Message";
+});
